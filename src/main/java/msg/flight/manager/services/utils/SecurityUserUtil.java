5375a8c5d4a8124c6b63ba3e09045fb4259734bf
Marinca Iasmina <iasmina.marinca@msg.group>
2024-08-14 10:31:26 +0300

    flight feature  pull request

--- src/main/java/msg/flight/manager/services/utils/SecurityUserUtil.java
+++ src/main/java/msg/flight/manager/services/utils/SecurityUserUtil.java
@@ -1,8 +1,6 @@
 package msg.flight.manager.services.utils;
 
-import lombok.experimental.UtilityClass;
 import msg.flight.manager.security.SecurityUser;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
